c02d2127ddc8ea88b19fd3b68d1982b3ec6f2b19
Oleg Ketrar <oleg.ketrar.dev@icloud.com>
2020-09-22 21:53:58 +0300

    add MailComposeViewController

---
+++ Sources/Tools/UIKit/MailComposeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MailComposeViewController.swift
+//  ToolsUIKit
+//
+//  Created by Oleg Ketrar on 04.07.17.
+//  Copyright © 2017 Oleg Ketrar. All rights reserved.
+//
+import Foundation
+import MessageUI
+
+/// Convenience Block-based `MFMailComposeViewController`.
+/// `delegate` property used by itself.
+/// You should not change `delegate` after initialization with `convenience init(_ callback:)`.
+/// But you still can use it as normal `MFMailComposeViewController` by creating with `designated` initializer.
+public final class MailComposeViewController: MFMailComposeViewController {
+
+    private var callbackClosure: (
+        MFMailComposeViewController,
+        MFMailComposeResult,
+        Error?) -> Void = { _, _, _ in }
+
+    /// - parameter callback: will be called instead of `mailComposeController(_ controller: didFinishWith:)`.
+    public convenience init(
+        _ callback: @escaping (
+            MFMailComposeViewController,
+            MFMailComposeResult,
+            Error?
+        ) -> Void) {
+
+        self.init()
+
+        callbackClosure = callback
+        mailComposeDelegate = self
+    }
+}
+
+// MARK: - MFMailComposeViewControllerDelegate
+
+extension MailComposeViewController: MFMailComposeViewControllerDelegate {
+
+    public func mailComposeController(
+        _ controller: MFMailComposeViewController,
+        didFinishWith result: MFMailComposeResult,
+        error: Error?) {
+
+        callbackClosure(controller, result, error)
+    }
+}
